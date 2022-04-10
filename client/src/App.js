c56aa03ed8711313e106be292c7a1c513e8cb10d
StefanUngureanul24 <unsugreanu_stefan_lucian@yahoo.com>
2022-04-10 16:19:59 +0200

    Creation of mobile interface - not final, but pretty close, TO DO: button and alignment of material select

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@
 
 import React, { useReducer, useState } from "react";
 import ReactDOM from 'react-dom';
+import { v4 as uuidv4 } from 'uuid';
 import Material from "./components/Material";
 
 import './styles/Material.css'
@@ -24,7 +25,12 @@ function App() {
       .then(data => setData(data.message));
   }, []);
   
-  const [formData, setFormData] = useReducer(formReducer, {});
+  const initialList = {};
+
+  const [list, setList] = React.useState(initialList);
+  const [formData, setFormData] = useReducer(formReducer, {
+    Menge: 1,
+  });
   const [submitting, setSubmitting] = useState(false);
 
   const handleSubmit = event => {
@@ -39,15 +45,23 @@ function App() {
     });
   }
 
+  /*
+  function handleAdd() {
+    const newList = list.concat({ name, id: uuidv4() });
+  
+    setList(newList);
+  }
+  */
+
   return (
     
     <div className="wrapper">
-      <h1>Material Erfassung</h1>
+      <h2>Material Erfassung</h2>
       {submitting &&
         <div>
           <ul id="prodList">
             {Object.entries(formData).map(([name, values]) => (
-              <li key={name}><p>{name}:{values.toString()}</p></li>
+              <li key={name}><p>{name} : {values.toString()}</p></li>
             ))}
           </ul>
         </div>
@@ -57,25 +71,25 @@ function App() {
         }
           <label>
             <p>material</p>
-            <select name="Material" onChange={handleChange}>
+            <select className="selectMaterial" name="Material" onChange={handleChange} value={formData.Material} >
               <option value="">Ein materiell zu wählen</option>
-              <option value="mat1">Material 1</option>
-              <option value="mat2">Material 2</option>
-              <option value="mat3">Material 3</option>
-              <option value="mat4">Material 4</option>
+              <option value="Material 1">Material 1</option>
+              <option value="Material 2">Material 2</option>
+              <option value="Material 3">Material 3</option>
+              <option value="Material 4">Material 4</option>
             </select>
           </label>
           <label>
-            <input name="Chargen" onChange={handleChange} />
+            <input name="Chargen" onChange={handleChange} value={formData.Chargen} />
             <p>chargen-nr.</p>
           </label>
           <label>
-            <input type="number" name="Menge" onChange={handleChange} />
+            <input type="number" name="Menge" onChange={handleChange} value={formData.Menge} />
             <p>menge</p>
           </label>
         {//</fieldset>
         }
-        <button type="submit">Speichern</button>
+        <button className="buttonForm" type="submit">Speichern</button>
       </form>
     </div>
     
